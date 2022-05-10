4aa7b1401f2c147e8ad843670fd437c6c11a1c3e
lamafab <42901763+lamafab@users.noreply.github.com>
2022-05-10 16:26:30 +0200

    read account key from config with supplied password

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import { load } from 'js-yaml';
 import { readFileSync } from 'fs';
 import { parse } from 'csv-parse';
 import { ApiPromise, WsProvider } from '@polkadot/api';
+import { KeyringPair, KeyringPair$Json } from '@polkadot/keyring/types';
+import {Keyring} from '@polkadot/keyring';
 
 type Config = {
 	end_point: string;
-	walletFilePath: string;
 	actionFilePath: string;
+	keystore: Keystore;
+}
+
+type Keystore = {
+  walletFilePath: string;
+  password: string;
 }
 
 type Record = {
@@ -17,7 +24,7 @@ type Record = {
 
 export const startAction = async (args: { config: string }): Promise<void> => {
 	console.log("Reading config from file", args.config);
-	const config = load(await readFileSync(args.config, "utf8")) as Config;
+	const config = load(readFileSync(args.config, "utf8")) as Config;
 
 	console.log("Reading from file", config.actionFilePath);
 	let content = readFileSync(config.actionFilePath, 'utf8');
@@ -36,6 +43,11 @@ export const startAction = async (args: { config: string }): Promise<void> => {
 	parser.end();
 
 	console.log(`Parsed ${records.length} CSV entries`);
+
+	const keyring = new Keyring({ type: 'sr25519' });
+	const json = JSON.parse(readFileSync(config.keystore.walletFilePath, 'utf8'));
+	const keystore = keyring.addFromJson(json);
+	keystore.decodePkcs8(config.keystore.password);
 }
 
 const command = new Command()
