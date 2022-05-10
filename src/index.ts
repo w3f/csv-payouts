5253340be7d24c370cc4a015d2950fecd25e1da1
lamafab <42901763+lamafab@users.noreply.github.com>
2022-05-10 16:14:14 +0200

    parse config file as yaml, separate actions and wallet file

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,46 @@
 import { Command } from 'commander';
+import { load } from 'js-yaml';
 import { readFileSync } from 'fs';
 import { parse } from 'csv-parse';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
+type Config = {
+	end_point: string;
+	walletFilePath: string;
+	actionFilePath: string;
+}
+
 type Record = {
-	from: string;
 	to: string;
 	amount: number;
 }
 
-export const startAction = async (args: { path: string }): Promise<void> => {
-	console.log("Reading from file:", args.path);
+export const startAction = async (args: { config: string }): Promise<void> => {
+	console.log("Reading config from file", args.config);
+	const config = load(await readFileSync(args.config, "utf8")) as Config;
 
-	let content = readFileSync(args.path, 'utf8');
-	console.log("Content:\n" + content);
+	console.log("Reading from file", config.actionFilePath);
+	let content = readFileSync(config.actionFilePath, 'utf8');
 
-	let entries: Record[] = [];
+	let records: Record[] = [];
 	const parser = parse({ delimiter: "," });
 	parser
 		.on('readable', (data) => {
 			let record;
 			while ((record = parser.read()) !== null) {
-				entries.push(record as Record);
+				records.push(record as Record);
 			}
 		});
 
 	parser.write(content);
 	parser.end();
 
-	console.log(entries);
+	console.log(`Parsed ${records.length} CSV entries`);
 }
 
 const command = new Command()
 	.description('Execute the CSV payouts')
-	.option('-p, --path [path]', 'Path to config file.', './config/main.csv')
+	.option('-c, --config [path]', 'Path to config file.', './config/main.csv')
 	.action(startAction);
 
 command.parse();
