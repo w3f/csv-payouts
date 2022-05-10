4e6c4d6c7581862e67430b3432e65dbe411a42ea
lamafab <42901763+lamafab@users.noreply.github.com>
2022-05-10 20:50:35 +0200

    run prettier

--- src/cache.ts
+++ src/cache.ts
@@ -1,121 +1,119 @@
-import { readFileSync, existsSync, writeFileSync } from 'fs';
-import { Hash } from '@polkadot/types/interfaces';
-import { Record, ToExecute } from './';
+import { readFileSync, existsSync, writeFileSync } from "fs";
+import { Hash } from "@polkadot/types/interfaces";
+import { Record, ToExecute } from "./";
 
 interface CacheEntry {
-	to: string;
-	amount: number;
-	txHash?: string;
-	date?: string;
+  to: string;
+  amount: number;
+  txHash?: string;
+  date?: string;
 }
 
 interface Skipped {
-	record: Record;
-	date: string;
+  record: Record;
+  date: string;
 }
 
 export interface Staged {
-	to_execute: ToExecute[],
-	skipped: Skipped[],
-	dangling: CacheEntry[],
+  to_execute: ToExecute[];
+  skipped: Skipped[];
+  dangling: CacheEntry[];
 }
 
 export class Cache {
-	path: string;
-	cache: CacheEntry[];
+  path: string;
+  cache: CacheEntry[];
 
-	constructor(path: string) {
-		var cache = [];
-		// If the cache file alreach exists, read from it.
-		if (existsSync(path)) {
-			cache = JSON.parse(readFileSync(path, 'utf8'));
-		}
+  constructor(path: string) {
+    var cache = [];
+    // If the cache file alreach exists, read from it.
+    if (existsSync(path)) {
+      cache = JSON.parse(readFileSync(path, "utf8"));
+    }
 
-		this.path = path;
-		this.cache = cache;
-	}
-	private _findTargetIndex(record: Record): number {
-		return this.cache.findIndex((cache_entry) => {
-			cache_entry.to == record.to &&
-				cache_entry.amount == record.amount
-		});
-	}
-	private _updateCache() {
-		// Overwrites the file with new data.
-		console.log(this.cache);
-		writeFileSync(this.path, JSON.stringify(this.cache));
-	}
-	public stageActions(records: Record[]): Staged {
-		let staged: Staged = {
-			to_execute: [],
-			skipped: [],
-			dangling: [],
-		};
+    this.path = path;
+    this.cache = cache;
+  }
+  private _findTargetIndex(record: Record): number {
+    return this.cache.findIndex((cache_entry) => {
+      cache_entry.to == record.to && cache_entry.amount == record.amount;
+    });
+  }
+  private _updateCache() {
+    // Overwrites the file with new data.
+    console.log(this.cache);
+    writeFileSync(this.path, JSON.stringify(this.cache));
+  }
+  public stageActions(records: Record[]): Staged {
+    let staged: Staged = {
+      to_execute: [],
+      skipped: [],
+      dangling: [],
+    };
 
-		records.forEach((record: Record) => {
-			// Check if the record is already cached.
-			const idx = this._findTargetIndex(record)
+    records.forEach((record: Record) => {
+      // Check if the record is already cached.
+      const idx = this._findTargetIndex(record);
 
-			// If the record _is_ already cached...
-			if (idx != -1) {
-				// and it was already executed...
-				let entry = this.cache[idx];
-				if (entry?.txHash) {
-					staged.skipped.push({
-						record: record,
-						// TODO
-						date: "",
-					});
-				}
-				// otherwise prepare it for execution.
-				else {
-					staged.to_execute.push(record);
-				}
-			}
-			// and if not...
-			else {
-				console.log(record);
-				this.cache.push({
-					to: record.to,
-					amount: record.amount,
-					txHash: undefined,
-					date: undefined,
-				});
+      // If the record _is_ already cached...
+      if (idx != -1) {
+        // and it was already executed...
+        let entry = this.cache[idx];
+        if (entry?.txHash) {
+          staged.skipped.push({
+            record: record,
+            // TODO
+            date: "",
+          });
+        }
+        // otherwise prepare it for execution.
+        else {
+          staged.to_execute.push(record);
+        }
+      }
+      // and if not...
+      else {
+        console.log(record);
+        this.cache.push({
+          to: record.to,
+          amount: record.amount,
+          txHash: undefined,
+          date: undefined,
+        });
 
-				staged.to_execute.push(record);
-			}
-		});
+        staged.to_execute.push(record);
+      }
+    });
 
-		// Find any dangling entries, i.e. entries that have been cached, but
-		// not executed, and are currently not in the action file.
-		this.cache
-			.filter((entry) => !entry.txHash)
-			.forEach((entry) => {
-				let found = records.find((record) => {
-					entry.to == record.to &&
-						entry.amount == record.amount
-				});
+    // Find any dangling entries, i.e. entries that have been cached, but
+    // not executed, and are currently not in the action file.
+    this.cache
+      .filter((entry) => !entry.txHash)
+      .forEach((entry) => {
+        let found = records.find((record) => {
+          entry.to == record.to && entry.amount == record.amount;
+        });
 
-				if (!found) {
-					staged.dangling.push(entry);
-				}
-			});
+        if (!found) {
+          staged.dangling.push(entry);
+        }
+      });
 
-		this._updateCache();
+    this._updateCache();
 
-		return staged
-	}
-	public trackExecution(record: ToExecute, txHash: Hash) {
-		const idx = this._findTargetIndex(record);
+    return staged;
+  }
+  public trackExecution(record: ToExecute, txHash: Hash) {
+    const idx = this._findTargetIndex(record);
 
-		if (idx) {
-			this.cache[idx].txHash = txHash.toString();
-			// TODO
-			this.cache[idx].date = "";
-		} else {
-			// TODO: FATAL
-		}
+    if (idx) {
+      this.cache[idx].txHash = txHash.toString();
+      // TODO
+      this.cache[idx].date = "";
+    } else {
+      // TODO: FATAL
+    }
 
-		this._updateCache();
-	}
+    this._updateCache();
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,96 @@
-import { Command } from 'commander';
-import { load } from 'js-yaml';
-import { readFileSync, createWriteStream, existsSync, WriteStream } from 'fs';
-import { parse } from 'csv-parse';
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { KeyringPair, KeyringPair$Json } from '@polkadot/keyring/types';
-import { Keyring } from '@polkadot/keyring';
-import { createLogger } from '@w3f/logger';
-import { Cache } from './cache';
+import { Command } from "commander";
+import { load } from "js-yaml";
+import { readFileSync, createWriteStream, existsSync, WriteStream } from "fs";
+import { parse } from "csv-parse";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { KeyringPair, KeyringPair$Json } from "@polkadot/keyring/types";
+import { Keyring } from "@polkadot/keyring";
+import { createLogger } from "@w3f/logger";
+import { Cache } from "./cache";
 
-const CACHE_PATH = '.action_cache.json';
+const CACHE_PATH = ".action_cache.json";
 
 interface Config {
-	end_point: string;
-	actionFilePath: string;
-	keystore: Keystore;
+  end_point: string;
+  actionFilePath: string;
+  keystore: Keystore;
 }
 
 interface Keystore {
-	walletFilePath: string;
-	password: string;
+  walletFilePath: string;
+  password: string;
 }
 
 export interface Record {
-	to: string;
-	amount: number;
+  to: string;
+  amount: number;
 }
 
 export interface ToExecute {
-	to: string;
-	amount: number;
+  to: string;
+  amount: number;
 }
 
 function abort() {
-	process.exit(1);
+  process.exit(1);
 }
 
 const start = async (args: { config: string }): Promise<void> => {
-	const log = createLogger("debug");
+  const log = createLogger("debug");
 
-	// Parse Config
-	log.debug(`Reading config from file ${args.config}`);
-	const config = load(readFileSync(args.config, "utf8")) as Config;
+  // Parse Config
+  log.debug(`Reading config from file ${args.config}`);
+  const config = load(readFileSync(args.config, "utf8")) as Config;
 
-	// Parse CSV file
-	log.debug(`Reading from file ${config.actionFilePath}`);
-	let content = readFileSync(config.actionFilePath, 'utf8');
+  // Parse CSV file
+  log.debug(`Reading from file ${config.actionFilePath}`);
+  let content = readFileSync(config.actionFilePath, "utf8");
 
-	let records: Record[] = [];
-	const parser = parse({ delimiter: "," });
-	parser
-		.on('readable', () => {
-			let record;
-			while ((record = parser.read()) !== null) {
-				records.push(record as Record);
-			}
-		});
+  let records: Record[] = [];
+  const parser = parse({ delimiter: "," });
+  parser.on("readable", () => {
+    let record;
+    while ((record = parser.read()) !== null) {
+      records.push(record as Record);
+    }
+  });
 
-	parser.write(content);
-	parser.end();
+  parser.write(content);
+  parser.end();
 
-	log.info(`Parsed ${records.length} CSV entries`);
+  log.info(`Parsed ${records.length} CSV entries`);
 
-	// Parse and decode provided account.
-	log.debug(`Reading account key from ${config.keystore.walletFilePath}`);
-	const keyring = new Keyring({ type: 'sr25519' });
-	const json = JSON.parse(readFileSync(config.keystore.walletFilePath, 'utf8'));
-	const account = keyring.addFromJson(json);
-	account.decodePkcs8(config.keystore.password);
+  // Parse and decode provided account.
+  log.debug(`Reading account key from ${config.keystore.walletFilePath}`);
+  const keyring = new Keyring({ type: "sr25519" });
+  const json = JSON.parse(readFileSync(config.keystore.walletFilePath, "utf8"));
+  const account = keyring.addFromJson(json);
+  account.decodePkcs8(config.keystore.password);
 
-	if (account.isLocked) {
-		log.error("Failed to initialize keystore, account is locked");
-		abort();
-	}
+  if (account.isLocked) {
+    log.error("Failed to initialize keystore, account is locked");
+    abort();
+  }
 
-	// Init caching.
-	let cache = new Cache(CACHE_PATH);
-	const { to_execute, skipped, dangling } = cache.stageActions(records);
+  // Init caching.
+  let cache = new Cache(CACHE_PATH);
+  const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-	if (dangling.length != 0) {
-		log.warn(
-			`There are ${dangling.length} staged actions there weren't executed yet and are no longer present in the action file:`
-		);
+  if (dangling.length != 0) {
+    log.warn(
+      `There are ${dangling.length} staged actions there weren't executed yet and are no longer present in the action file:`
+    );
 
-		dangling.forEach((entry) => {
-			log.warn(`To: ${entry.to}, amount: ${entry.amount}`);
-		});
+    dangling.forEach((entry) => {
+      log.warn(`To: ${entry.to}, amount: ${entry.amount}`);
+    });
 
-		log.error(`Please fix the issue or run the 'unstage' command.`);
-		abort();
-	}
+    log.error(`Please fix the issue or run the 'unstage' command.`);
+    abort();
+  }
 
-	// Initialize RPC endpoint.
-		/*
+  // Initialize RPC endpoint.
+  /*
 	const wsProvider = new WsProvider(config.end_point);
 	const api = await ApiPromise.create({ provider: wsProvider });
 
@@ -105,11 +104,11 @@ const start = async (args: { config: string }): Promise<void> => {
 		cache.trackExecution(entry, txHash);
 	}
 		*/
-}
+};
 
 const command = new Command()
-	.description('Execute the CSV payouts')
-	.option('-c, --config [path]', 'Path to config file.', './config/main.csv')
-	.action(start);
+  .description("Execute the CSV payouts")
+  .option("-c, --config [path]", "Path to config file.", "./config/main.csv")
+  .action(start);
 
 command.parse();
