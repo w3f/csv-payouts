6ae7d06cf3ef4cc84ade131750710efbaa668258
lamafab <42901763+lamafab@users.noreply.github.com>
2022-05-10 15:14:21 +0200

    read from file, parse CSV

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 import { Command } from 'commander';
+import { readFileSync } from 'fs';
+import { parse } from 'csv-parse';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
-export const startAction = async (cmd: { config: string }): Promise<void> =>{}
+type Record = {
+	from: string;
+	to: string;
+	amount: number;
+}
 
-new Command()
-  .description('Execute the CSV payouts')
-  .option('-c, --config [path]', 'Path to config file.', './config/main.csv')
-  .action(startAction);
+export const startAction = async (args: { path: string }): Promise<void> => {
+	console.log("Reading from file:", args.path);
+
+	let content = readFileSync(args.path, 'utf8');
+	console.log("Content:\n" + content);
+
+	let entries: Record[] = [];
+	const parser = parse({ delimiter: "," });
+	parser
+		.on('readable', (data) => {
+			let record;
+			while ((record = parser.read()) !== null) {
+				entries.push(record as Record);
+			}
+		});
+
+	parser.write(content);
+	parser.end();
+
+	console.log(entries);
+}
+
+const command = new Command()
+	.description('Execute the CSV payouts')
+	.option('-p, --path [path]', 'Path to config file.', './config/main.csv')
+	.action(startAction);
+
+command.parse();
