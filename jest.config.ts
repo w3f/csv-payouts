2169372b54d62cce2b7c993b1ba8dfa1aab28ad9
lamafab <42901763+lamafab@users.noreply.github.com>
2022-05-11 20:01:37 +0200

    run prettier

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
-import type {Config} from '@jest/types';
+import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
   transform: {
-       '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
   },
 };
 
--- src/cache.test.ts
+++ src/cache.test.ts
@@ -1,169 +1,176 @@
 import { Record } from "./";
 import { Cache } from "./cache";
 
-const CACHE_PATH = '/tmp';
+const CACHE_PATH = "/tmp";
 
 function record_samples(): Array<Record> {
-	return [
-		{
-			to: "Alice",
-			amount: 100
-		},
-		{
-			to: "Bob",
-			amount: 200
-		},
-		{
-			to: "Eve",
-			amount: 300
-		}
-	];
+  return [
+    {
+      to: "Alice",
+      amount: 100,
+    },
+    {
+      to: "Bob",
+      amount: 200,
+    },
+    {
+      to: "Eve",
+      amount: 300,
+    },
+  ];
 }
 
-test('newRecordInserts', () => {
-	const cache = new Cache();
+test("newRecordInserts", () => {
+  const cache = new Cache();
 
-	const records = record_samples();
-	const alice = records[0];
-	const bob = records[1];
-	const eve = records[2];
+  const records = record_samples();
+  const alice = records[0];
+  const bob = records[1];
+  const eve = records[2];
 
-	const { to_execute, skipped, dangling } = cache.stageActions(records);
+  const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-	expect(to_execute.length).toEqual(3);
-	expect(skipped.length).toEqual(0);
-	expect(dangling.length).toEqual(0);
+  expect(to_execute.length).toEqual(3);
+  expect(skipped.length).toEqual(0);
+  expect(dangling.length).toEqual(0);
 
-	expect(to_execute).toContain(alice);
-	expect(to_execute).toContain(bob);
-	expect(to_execute).toContain(eve);
-})
+  expect(to_execute).toContain(alice);
+  expect(to_execute).toContain(bob);
+  expect(to_execute).toContain(eve);
+});
 
-test('nonExecutedRecordInserts', () => {
-	const cache = new Cache();
+test("nonExecutedRecordInserts", () => {
+  const cache = new Cache();
 
-	const records = record_samples();
-	const alice = records[0];
-	const bob = records[1];
-	const eve = records[2];
+  const records = record_samples();
+  const alice = records[0];
+  const bob = records[1];
+  const eve = records[2];
 
-	{
-		const { to_execute, skipped, dangling } = cache.stageActions([alice, eve]);
+  {
+    const { to_execute, skipped, dangling } = cache.stageActions([alice, eve]);
 
-		expect(to_execute.length).toEqual(2);
-		expect(skipped.length).toEqual(0);
-		expect(dangling.length).toEqual(0);
+    expect(to_execute.length).toEqual(2);
+    expect(skipped.length).toEqual(0);
+    expect(dangling.length).toEqual(0);
 
-		expect(to_execute).toContain(alice);
-		expect(to_execute).toContain(eve);
-	}
+    expect(to_execute).toContain(alice);
+    expect(to_execute).toContain(eve);
+  }
 
-	{
-		// Reinserts alice and eve, bob is newly inserted.
-		const { to_execute, skipped, dangling } = cache.stageActions(records);
+  {
+    // Reinserts alice and eve, bob is newly inserted.
+    const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-		// No executions have been tracked yet, therefore all are marked for
-		// execution.
-		expect(to_execute.length).toEqual(3);
-		expect(skipped.length).toEqual(0);
-		expect(dangling.length).toEqual(0);
+    // No executions have been tracked yet, therefore all are marked for
+    // execution.
+    expect(to_execute.length).toEqual(3);
+    expect(skipped.length).toEqual(0);
+    expect(dangling.length).toEqual(0);
 
-		expect(to_execute).toContain(alice);
-		expect(to_execute).toContain(bob);
-		expect(to_execute).toContain(eve);
-	}
-})
+    expect(to_execute).toContain(alice);
+    expect(to_execute).toContain(bob);
+    expect(to_execute).toContain(eve);
+  }
+});
 
-test('skipExecuted', () => {
-	const cache = new Cache();
+test("skipExecuted", () => {
+  const cache = new Cache();
 
-	const records = record_samples();
-	const alice = records[0];
-	const bob = records[1];
-	const eve = records[2];
+  const records = record_samples();
+  const alice = records[0];
+  const bob = records[1];
+  const eve = records[2];
 
-	{
-		const { to_execute, skipped, dangling } = cache.stageActions(records);
+  {
+    const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-		expect(to_execute.length).toEqual(3);
-		expect(skipped.length).toEqual(0);
-		expect(dangling.length).toEqual(0);
+    expect(to_execute.length).toEqual(3);
+    expect(skipped.length).toEqual(0);
+    expect(dangling.length).toEqual(0);
 
-		expect(to_execute).toContain(alice);
-		expect(to_execute).toContain(bob);
-		expect(to_execute).toContain(eve);
-	}
+    expect(to_execute).toContain(alice);
+    expect(to_execute).toContain(bob);
+    expect(to_execute).toContain(eve);
+  }
 
-	// Mark alice and eve as executed.
-	cache.trackExecution(alice, "hash_a");
-	cache.trackExecution(eve, "hash_b");
+  // Mark alice and eve as executed.
+  cache.trackExecution(alice, "hash_a");
+  cache.trackExecution(eve, "hash_b");
 
-	{
-		const { to_execute, skipped, dangling } = cache.stageActions(records);
+  {
+    const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-		expect(to_execute.length).toEqual(1);
-		expect(skipped.length).toEqual(2);
-		expect(dangling.length).toEqual(0);
+    expect(to_execute.length).toEqual(1);
+    expect(skipped.length).toEqual(2);
+    expect(dangling.length).toEqual(0);
 
-		expect(to_execute).toContain(bob);
+    expect(to_execute).toContain(bob);
 
-		expect(skipped[0]).toMatchObject({ record: alice, txHash: "hash_a"});
-		expect(skipped[1]).toMatchObject({ record: eve, txHash: "hash_b"});
+    expect(skipped[0]).toMatchObject({ record: alice, txHash: "hash_a" });
+    expect(skipped[1]).toMatchObject({ record: eve, txHash: "hash_b" });
 
-		// Date is set.
-		expect(skipped[0].date).toBeDefined();
-		expect(skipped[1].date).toBeDefined();
-	}
+    // Date is set.
+    expect(skipped[0].date).toBeDefined();
+    expect(skipped[1].date).toBeDefined();
+  }
 
-	// Mark bob as executed.
-	cache.trackExecution(bob, "hash_c");
+  // Mark bob as executed.
+  cache.trackExecution(bob, "hash_c");
 
-	{
-		const { to_execute, skipped, dangling } = cache.stageActions(records);
+  {
+    const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-		// All have been marked as executed now.
-		expect(to_execute.length).toEqual(0);
-		expect(skipped.length).toEqual(3);
-		expect(dangling.length).toEqual(0);
+    // All have been marked as executed now.
+    expect(to_execute.length).toEqual(0);
+    expect(skipped.length).toEqual(3);
+    expect(dangling.length).toEqual(0);
 
-		expect(skipped[0]).toMatchObject({ record: alice, txHash: "hash_a"});
-		expect(skipped[1]).toMatchObject({ record: bob, txHash: "hash_c"});
-		expect(skipped[2]).toMatchObject({ record: eve, txHash: "hash_b"});
-	}
-})
+    expect(skipped[0]).toMatchObject({ record: alice, txHash: "hash_a" });
+    expect(skipped[1]).toMatchObject({ record: bob, txHash: "hash_c" });
+    expect(skipped[2]).toMatchObject({ record: eve, txHash: "hash_b" });
+  }
+});
 
+test("danglingActions", () => {
+  const cache = new Cache();
 
-test('danglingActions', () => {
-	const cache = new Cache();
+  const records = record_samples();
+  const alice = records[0];
+  const bob = records[1];
+  const eve = records[2];
 
-	const records = record_samples();
-	const alice = records[0];
-	const bob = records[1];
-	const eve = records[2];
+  {
+    const { to_execute, skipped, dangling } = cache.stageActions(records);
 
-	{
-		const { to_execute, skipped, dangling } = cache.stageActions(records);
+    expect(to_execute.length).toEqual(3);
+    expect(skipped.length).toEqual(0);
+    expect(dangling.length).toEqual(0);
 
-		expect(to_execute.length).toEqual(3);
-		expect(skipped.length).toEqual(0);
-		expect(dangling.length).toEqual(0);
+    expect(to_execute).toContain(alice);
+    expect(to_execute).toContain(bob);
+    expect(to_execute).toContain(eve);
+  }
 
-		expect(to_execute).toContain(alice);
-		expect(to_execute).toContain(bob);
-		expect(to_execute).toContain(eve);
-	}
+  {
+    // Only bob is reinserted, alice and eve are missing.
+    const { to_execute, skipped, dangling } = cache.stageActions([bob]);
 
-	{
-		// Only bob is reinserted, alice and eve are missing.
-		const { to_execute, skipped, dangling } = cache.stageActions([bob]);
+    expect(to_execute.length).toEqual(1);
+    expect(skipped.length).toEqual(0);
+    expect(dangling.length).toEqual(2);
 
-		expect(to_execute.length).toEqual(1);
-		expect(skipped.length).toEqual(0);
-		expect(dangling.length).toEqual(2);
+    expect(to_execute).toContain(bob);
 
-		expect(to_execute).toContain(bob);
-
-		expect(dangling[0]).toMatchObject({ to: alice.to, amount: alice.amount, txHash: null});
-		expect(dangling[1]).toMatchObject({ to: eve.to, amount: eve.amount, txHash: null});
-	}
-})
+    expect(dangling[0]).toMatchObject({
+      to: alice.to,
+      amount: alice.amount,
+      txHash: null,
+    });
+    expect(dangling[1]).toMatchObject({
+      to: eve.to,
+      amount: eve.amount,
+      txHash: null,
+    });
+  }
+});
